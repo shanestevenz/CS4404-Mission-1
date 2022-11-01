cb42d68565b0ccc7cd0a520f0ca71812e117bdef
shane <shanestevenz@gmail.com>
2022-11-01 17:27:16 -0400

    started on backend

---
+++ public/result.js
@@ -0,0 +1,41 @@
+
+fetch("/getResults", {
+})
+    .then(function (response) {
+        return response.json();
+    })
+    .then(db => {
+        //TODO render as  results
+    });
+
+
+
+
+function CastVote() {
+
+  //  let candidate = todoForm.elements.todo,
+  
+        
+        let json = {
+            candidate: 1, //candidate and how much we increment by
+        },
+        body = JSON.stringify(json)
+
+
+    fetch('/vote', {
+        method: 'POST',
+        body,
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+        .then(function (response) {
+            // do something with the reponse
+            return response.json();
+        })
+
+        .then(function (data) {
+           // document.querySelector("form").reset();
+            // addRowToTable (data);
+        });
+}
--- server.js
+++ server.js
@@ -5,13 +5,53 @@ const path = require("path");
 
 app.use(cors());
 
+
+
+const mongodb= require('mongodb');
+const uri = "mongodb+srv://Misison1:Mission1@cluster0.ewyurrb.mongodb.net/?retryWrites=true&w=majority"; //username: Misison1    password: Mission1
+const client = new mongodb.MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true});
+
+let collection
+
+
+client.connect()
+  .then(() => {
+    return client.db('networkSecurity_data').collection('networkSecurity_collection')
+  })
+  .then(__collection => {
+    collection = __collection
+    return collection.find({}).toArray()
+  })
+  .then(console.log)
+
+
+
+
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
+app.get("/result", function (req, res) {
+  res.sendFile(path.join(__dirname, "public", "result.html"));
+});
+
+
+app.get("/getResults", function (req, res) {
+    
+  if (collection !== null) {
+    
+    //TODO: dont pass username here: just get everyting
+    collection.find({username: req.session.username}).toArray().then(result => res.json(result))
+  }
+});
+
+
 
 app.use("/vote", (req, res) => {
- 
+    console.log("Cast Vote")
     console.log(req.body.candidate)
+
+    collection.insertOne(request.body).then(result => collection.findOne(result.insertedId)).then( findResult => response.json( findResult))
+    
 });
 
 //add other API calls
